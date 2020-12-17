63d2c794d78bb0422c5504ffcc5b270829a4dbc0
knipferrc <knipferrc@gmail.com>
2020-12-16 20:22:00 -0500

    WIP page layouts

--- public/pages/home/index.tsx
+++ public/pages/home/index.tsx
@@ -9,9 +9,28 @@ const cx = classnames.bind(styles);
 
 export const Home: FunctionComponent = () => {
   return (
-    <section class={cx('container')}>
-      <h1 class={cx('header-text')}>Tyler Knipfer</h1>
-      <img class={cx('profile-image')} src={profilePic} />
-    </section>
+    <>
+      <video
+        class={cx('background')}
+        playsInline
+        autoPlay
+        muted
+        loop
+        poster="https://media.giphy.com/media/3o75248xZ4uXdJbHbi/giphy.mp4"
+        id="bgvid"
+      >
+        <source
+          src="https://media.giphy.com/media/3o75248xZ4uXdJbHbi/giphy.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <section class={cx('container')}>
+        <h1 class={cx('header-text')}>Tyler Knipfer</h1>
+        <img class={cx('profile-image')} src={profilePic} />
+        <p class={cx('intro-text')}>
+          Hi, I'm Tyler, a Frontend Engineer from Cincinnati
+        </p>
+      </section>
+    </>
   );
 };
