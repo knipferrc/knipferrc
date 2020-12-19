3f315d662a369c0b19c3be75517224cce05e9578
knipferrc <knipferrc@gmail.com>
2020-12-19 13:51:36 -0500

    update experience page

--- public/pages/experience/index.tsx
+++ public/pages/experience/index.tsx
@@ -1,10 +1,35 @@
 import { FunctionComponent } from 'preact';
+import classnames from 'classnames/bind';
+
+import styles from './style.module.css';
+
+const cx = classnames.bind(styles);
 
 const Experience: FunctionComponent = () => {
   return (
-    <>
-      <h1>Experience</h1>
-    </>
+    <section class={cx('container')}>
+      <h1 class={cx('heading')}>Work Experience</h1>
+      <div class={cx('jobs-container')}>
+        <div class={cx('job-card')}>
+          <h3 class={cx('subheader')}>Ascendum Solutions (Cincinnati, OH)</h3>
+          <p class={cx('description')}>
+            Engineering Manager October 2019 - Present
+          </p>
+        </div>
+        <div class={cx('job-card')}>
+          <h3 class={cx('subheader')}>Ascendum Solutions (Cincinnati, OH)</h3>
+          <p class={cx('description')}>
+            Sr. Software Engineer February 2018 - October 2019
+          </p>
+        </div>
+        <div class={cx('job-card')}>
+          <h3 class={cx('subheader')}>Millipore Sigma (St. Louis, MO)</h3>
+          <p class={cx('description')}>
+            Frontend Developer June 2016 - Janurary 2018
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- public/pages/home/index.tsx
+++ public/pages/home/index.tsx
@@ -9,14 +9,12 @@ const cx = classnames.bind(styles);
 
 export const Home: FunctionComponent = () => {
   return (
-    <>
-      <section class={cx('container')}>
-        <h1 class={cx('header-text')}>Tyler Knipfer</h1>
-        <img class={cx('profile-image')} src={profilePic} alt="profile" />
-        <p class={cx('intro-text')}>
-          Hi, I'm Tyler, a Frontend Engineer from Cincinnati
-        </p>
-      </section>
-    </>
+    <section class={cx('container')}>
+      <h1 class={cx('header-text')}>Tyler Knipfer</h1>
+      <img class={cx('profile-image')} src={profilePic} alt="profile" />
+      <p class={cx('intro-text')}>
+        Hi, I'm Tyler, a Frontend Engineer from Cincinnati
+      </p>
+    </section>
   );
 };
