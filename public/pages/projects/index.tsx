f345e828c447f05001694d45e3dc5653f17fef4b
knipferrc <knipferrc@gmail.com>
2021-01-03 15:39:43 -0500

    setup projects page

--- public/pages/projects/index.tsx
+++ public/pages/projects/index.tsx
@@ -1,10 +1,33 @@
 import { FunctionComponent } from 'preact';
+import classnames from 'classnames/bind';
+
+import styles from './style.module.css';
+
+const cx = classnames.bind(styles);
 
 const Projects: FunctionComponent = () => {
   return (
-    <>
-      <h1>Projects</h1>
-    </>
+    <section class={cx('container')}>
+      <h1 class={cx('heading')}>Projects</h1>
+      <div class={cx('projects-container')}>
+        <div class={cx('project-card')}>
+          <h3 class={cx('subheader')}>
+            Plate{' '}
+            <a
+              className={cx('app-link')}
+              target="_blank"
+              rel="noreferrer"
+              href="https://plate-app.vercel.app"
+            >
+              https://plate-app.vercel.app
+            </a>
+          </h3>
+          <p class={cx('description')}>
+            Web app to keep track of things you need to do
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
